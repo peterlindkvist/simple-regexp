9a5b2b0c72f18e0431cf455cd5a53bb12e9cab67
Peter Lindkvist <peter.lindkvist@doberman.se>
2015-10-20 15:00:39 +0200

    updated test docs

--- test/srxp.unittest.js
+++ test/srxp.unittest.js
@@ -4,24 +4,24 @@ var srxp = require('../src/srxp.js');
 
 describe('Simple Regexp', function(){
   describe('simplify', function(){
-    it('return same simple string', function(){
+    it('should return the same simple string', function(){
       assert.equal(srxp.simplify('abc'), 'abc');
       assert.equal(srxp.simplify('123'), '123');
     });
 
-    it('return string without special chars', function(){
+    it('should return a string without special chars', function(){
       assert.equal(srxp.simplify('åäöéÅÄÖü'), 'aaoeAAOu');
       assert.equal(srxp.simplify("a   b  \t c"), 'a b c');
     });
 
-    it('return string without double spaces', function(){
+    it('should a return string without double spaces', function(){
       assert.equal(srxp.simplify('abc'), 'abc');
       assert.equal(srxp.simplify("a   b  \t c"), 'a b c');
     });
   });
 
   describe('trim', function(){
-    it('remove spaces and tabs in at start and end of strings', function(){
+    it('should remove spaces and tabs in at start and end of strings and join multiple spaces to one', function(){
       assert.equal(srxp.trim('abc'), 'abc');
       assert.equal(srxp.trim(" abc "), 'abc');
       assert.equal(srxp.trim("\t  abc\t  "), 'abc');
@@ -30,7 +30,7 @@ describe('Simple Regexp', function(){
 
   describe('expandPattern', function(){
 
-    it('fix spaces', function(){
+    it('should add regexp for multiple spaces', function(){
       assert.equal(srxp.expandPattern('a b'), 'a\\s{1,}b');
       assert.equal(srxp.expandPattern('a   b'), 'a\\s{1,}b');
       assert.equal(srxp.expandPattern('a   b c'), 'a\\s{1,}b\\s{1,}c');
@@ -38,7 +38,7 @@ describe('Simple Regexp', function(){
   });
 
   describe('match', function(){
-    it('simple string ', function(){
+    it('should find a simple string ', function(){
       var str = 'abcdefgabc';
       assert.deepEqual(srxp(str).match('abc').result(), ['abc', 'abc']);
       assert.deepEqual(srxp(str).match('cde').result(), ['cde']);
@@ -46,14 +46,14 @@ describe('Simple Regexp', function(){
 
     });
 
-    it('multiple strings ', function(){
+    it('should find multiple strings ', function(){
       var str = 'abcdefgabc';
       assert.deepEqual(srxp(str).match('abcd', 'ef').result(), ['abcd', 'ef']);
       assert.deepEqual(srxp(str).match('abc', 'ef').result(), ['abc', 'abc', 'ef']);
 
     });
 
-    it('simple regexp ', function(){
+    it('should find a match with regexp ', function(){
       var str = 'abcdefgabc';
       assert.deepEqual(srxp(str).match(/abc/g).result(), ['abc', 'abc']);
       assert.deepEqual(srxp(str).match(/a|b/g).result(), ['a', 'b', 'a', 'b']);
@@ -63,14 +63,14 @@ describe('Simple Regexp', function(){
 
     });
 
-    it('grouped regexp ', function(){
+    it('should find matches in a grouped regexp ', function(){
       var str = 'abcdefgabc';
       assert.deepEqual(srxp(str).match(/ab(c)/g).result(), ['c', 'c']);
       assert.deepEqual(srxp(str).match(/(ab(c))/g).result(), ['abc', 'c', 'abc', 'c']);
       assert.deepEqual(srxp(str).match(/((a)(.*)d)(.{3})/g).result(), ['abcd', 'a', 'bc', 'efg']);
     });
 
-    it('expanded searches', function(){
+    it('should find mathces in expanded searches', function(){
       var str = '<div  class = "test">content</div>';
       assert.deepEqual(srxp(str).match('<div').result(), ['<div']);
       assert.deepEqual(srxp(str).match('<div class').result(), ['<div  class']);
@@ -84,19 +84,18 @@ describe('Simple Regexp', function(){
     it('should find matches between simple strings ', function(){
       var str = 'abcdefgabc';
       assert.deepEqual(srxp(str).between('bc', 'gab').result(), ['def']);
-      //assert.deepEqual(srxp(str).between('a', 'c').result(), ['b', 'b']);
+      assert.deepEqual(srxp(str).between('a', 'c').result(), ['b', 'b']);
     });
 
     it('should find match paranteses ', function(){
       var str = 'a(b(c))(d)';
-      //assert.deepEqual(srxp(str).between('\\(', '\\)').result(), ['b(c)', 'c', 'd']);
-      //assert.deepEqual(srxp(str).between('b\\(', 'd\\)').result(), ['c))(']);
-
+      assert.deepEqual(srxp(str).between('\\(', '\\)').result(), ['b(c)', 'c', 'd']);
+      assert.deepEqual(srxp(str).between('b\\(', 'd\\)').result(), ['c))(']);
     });
   });
 
   describe('exclude', function(){
-    it('simple strings ', function(){
+    it('should exclude matches from result', function(){
       var str = 'abc aBc abc';
       assert.deepEqual(srxp(str).between('a','c').result(), ['b', 'B', 'b']);
       assert.deepEqual(srxp(str).between('a','c').exclude('B').result(), ['b', 'b']);
